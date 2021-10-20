aafe2a001e58472fa5c68b322d857e4417b4fd51
sandcr6 <csandeep6@gmail.com>
2021-10-20 06:55:07 +0000

    initial trash can inventory service commit

---
+++ inv.py
@@ -0,0 +1,19 @@
+""" Trash Can Inventory Service
+"""
+
+
+from flask import Flask
+from flask import jsonify
+from flask import request
+
+
+app = Flask(__name__)
+
+
+VERSION = "0.1"
+
+
+@app.route('/')
+def version():
+    """ Root IRI returns the API version """
+    return jsonify(version=VERSION)
